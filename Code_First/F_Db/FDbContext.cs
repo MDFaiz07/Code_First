69f6c79c82b73b0750fa44d8d0ee16fa70f036dd
admin <admin@CHETUIWK1144>
2022-04-14 15:15:13 +0530

    Add project files.

---
+++ Code_First/F_Db/FDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Code_First.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Code_First.F_Db
+{
+    public class FDbContext: DbContext
+    {
+        public FDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<CF_Model> Emp_Table { get; set; }
+
+    }
+}
